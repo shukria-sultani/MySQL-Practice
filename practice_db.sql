39a97fa6752bb4cf0f3463e994d9464474b69b13
Shukria <shukriasul5@gmail.com>
2026-05-30 18:06:26 +0430

    practice union operator

--- practice_db.sql
+++ practice_db.sql
@@ -14,8 +14,35 @@ INSERT INTO Bookings (BookingID, BookingDate, TableNumber, NumberOfGuests, custo
 
 
 
--- left join
+-- inner join
 
 SELECT FullName,PhoneNumber,BookingDate,NumberOfGuests from customers INNER JOIN Bookings on customers.customer_id = Bookings.customer_id
-
+-- left join
 select c.customer_id, b.BookingID from customers AS c LEFT join Bookings AS b on c.customer_id = b.customer_id
+
+ALTER TABLE customers RENAME To old_customers
+
+CREATE TABLE current_customers(customer_id INT NOT NULL PRIMARY KEY, FullName VARCHAR(100) NOT NULL, PhoneNumber INT NOT NULL UNIQUE)
+
+ALTER TABLE old_customers 
+ADD COLUMN location VARCHAR(300)
+
+UPDATE old_customers
+SET location = "US"
+WHERE customer_id = 1 OR customer_id = 2
+
+INSERT INTO current_customers(customer_id, FullName, PhoneNumber) VALUES (1, "Mahjabeen", 0778921456),(2, "Karim", 0789012345), (3, "Mohadisa", 0743423569), (4, "Latif", 0799078234)
+
+ALTER TABLE current_customers
+ADD COLUMN location VARCHAR(300)
+
+UPDATE current_customers
+SET location = "Afghanistan"
+WHERE customer_id IN(1,3)
+
+-- UNION OPERATOR : Used to combine the result sets of two or more SELECT queries into a single result set.
+SELECT customer_id, FullName from old_customers UNION SELECT customer_id, FullName from current_customers
+
+-- conditional combining 
+
+SELECT FullName, location FROM old_customers WHERE location = "US" UNION SELECT FullName, location FROM current_customers WHERE location =  "Afghanistan"
