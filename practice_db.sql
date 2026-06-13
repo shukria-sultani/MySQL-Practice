a5c966a9c688d929316e3977df6df7a31ad39013
Shukria <shukriasul5@gmail.com>
2026-06-13 16:13:07 +0430

    add simicolon at the end of each command to safely import the db file

--- practice_db.sql
+++ practice_db.sql
@@ -1,10 +1,10 @@
--- Active: 1771875585958@@127.0.0.1@3306@little_lemmon
+-- Active: 1771875585958@@127.0.0.1@3306
 DROP DATABASE IF EXISTS university_database;
-CREATE DATABASE little_lemmon
+CREATE DATABASE little_lemmon;
 
-use little_lemmon
+use little_lemmon;
 
-create table customers(customer_id INT NOT NULL PRIMARY KEY, FullName VARCHAR(100) NOT NULL, PhoneNumber INT NOT NULL UNIQUE)
+create table customers(customer_id INT NOT NULL PRIMARY KEY, FullName VARCHAR(100) NOT NULL, PhoneNumber INT NOT NULL UNIQUE);
 
 Insert into customers (customer_id, FullName, PhoneNumber) VALUES (1, "Vanessa McCarthy", 0757536378), (2, "Marcos Romero", 0757536379), (3, "Hiroki Yamane", 0757536376), (4, "Anna Iversen", 0757536375), (5, "Diana Pinto", 0757536374);
 
@@ -16,36 +16,35 @@ INSERT INTO Bookings (BookingID, BookingDate, TableNumber, NumberOfGuests, custo
 
 -- inner join
 
-SELECT FullName,PhoneNumber,BookingDate,NumberOfGuests from customers INNER JOIN Bookings on customers.customer_id = Bookings.customer_id
+SELECT FullName,PhoneNumber,BookingDate,NumberOfGuests from customers INNER JOIN Bookings on customers.customer_id = Bookings.customer_id;
 -- left join
-select c.customer_id, b.BookingID from customers AS c LEFT join Bookings AS b on c.customer_id = b.customer_id
+select c.customer_id, b.BookingID from customers AS c LEFT join Bookings AS b on c.customer_id = b.customer_id;
+ALTER TABLE customers RENAME To old_customers;
 
-ALTER TABLE customers RENAME To old_customers
-
-CREATE TABLE current_customers(customer_id INT NOT NULL PRIMARY KEY, FullName VARCHAR(100) NOT NULL, PhoneNumber INT NOT NULL UNIQUE)
+CREATE TABLE current_customers(customer_id INT NOT NULL PRIMARY KEY, FullName VARCHAR(100) NOT NULL, PhoneNumber INT NOT NULL UNIQUE);
 
 ALTER TABLE old_customers 
-ADD COLUMN location VARCHAR(300)
+ADD COLUMN location VARCHAR(300);
 
 UPDATE old_customers
 SET location = "US"
-WHERE customer_id = 1 OR customer_id = 2
+WHERE customer_id = 1 OR customer_id = 2;
 
-INSERT INTO current_customers(customer_id, FullName, PhoneNumber) VALUES (1, "Mahjabeen", 0778921456),(2, "Karim", 0789012345), (3, "Mohadisa", 0743423569), (4, "Latif", 0799078234)
+INSERT INTO current_customers(customer_id, FullName, PhoneNumber) VALUES (1, "Mahjabeen", 0778921456),(2, "Karim", 0789012345), (3, "Mohadisa", 0743423569), (4, "Latif", 0799078234);
 
 ALTER TABLE current_customers
-ADD COLUMN location VARCHAR(300)
+ADD COLUMN location VARCHAR(300);
 
 UPDATE current_customers
 SET location = "Afghanistan"
-WHERE customer_id IN(1,3)
+WHERE customer_id IN(1,3);
 
 -- UNION OPERATOR : Used to combine the result sets of two or more SELECT queries into a single result set.
-SELECT customer_id, FullName from old_customers UNION SELECT customer_id, FullName from current_customers
+SELECT customer_id, FullName from old_customers UNION SELECT customer_id, FullName from current_customers;
 
 -- conditional combining 
 
-SELECT FullName, location FROM old_customers WHERE location = "US" UNION SELECT FullName, location FROM current_customers WHERE location =  "Afghanistan"
+SELECT FullName, location FROM old_customers WHERE location = "US" UNION SELECT FullName, location FROM current_customers WHERE location =  "Afghanistan";
 
 CREATE TABLE Orders(OrderID INT, Department VARCHAR(100), OrderDate DATE, OrderQty INT, OrderTotal INT, PRIMARY KEY(OrderID));
 
@@ -53,14 +52,14 @@ INSERT INTO Orders VALUES(1,'Lawn Care','2022-05-05',12,500),(2,'Decking','2022-
 (12,'Decking','2022-06-25',100,1400),(13,'Compost and Stones','2022-05-30',15,700),(14,'Trees and Shrubs','2022-06-15',10,300),(15,'Garden Decor','2022-06-11',2,1250),(16,'Lawn Care','2022-06-10',12,500),(17,'Decking','2022-06-25',150,1450),(18,'Trees and Shrubs','2022-06-10',15,400),(19,'Lawn Care','2022-06-10',12,500),(20,'Decking','2022-06-25',150,1450),(21,'Decking','2022-06-25',150,1450);
 
 -- Task 1: Write a SQL SELECT statement to group all records that have the same order date.
-SELECT OrderDate FROM Orders GROUP BY OrderDate
+SELECT OrderDate FROM Orders GROUP BY OrderDate;
 
 -- Task 2: Write a SQL SELECT statement to retrieve the number of orders placed on the same day.
-SELECT OrderDate, COUNT(OrderDate) FROM Orders GROUP BY OrderDate
+SELECT OrderDate, COUNT(OrderDate) FROM Orders GROUP BY OrderDate;
 
 -- Task 3: Write a SQL SELECT statement to retrieve the total order quantities placed by each department.
- SELECT Department, SUM(OrderQty) FROM Orders GROUP BY Department
+ SELECT Department, SUM(OrderQty) FROM Orders GROUP BY Department;
 
  -- Task 4: Write a SQL SELECT statement to retrieve the number of orders placed on the same day between the following dates: 1st June 2022 and 30th June 2022.
 
- SELECT OrderDate, COUNT(OrderDate) FROM Orders GROUP BY OrderDate HAVING OrderDate BETWEEN "2022-06-01" AND "2022-06-30"
+ SELECT OrderDate, COUNT(OrderDate) FROM Orders GROUP BY OrderDate HAVING OrderDate BETWEEN "2022-06-01" AND "2022-06-30";
